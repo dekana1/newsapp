b05c24dfaf4f4b57060d6e91859a551f372e5538
Dekana <chinenyeudechukwuu@gmail.com>
2022-09-18 21:52:11 +0100

    Test api

--- newsapi/apis/views.py
+++ newsapi/apis/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions
+
+from .serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
+
+
+class UserViewset(viewsets.ModelViewSet):
+    
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewset(viewsets.ModelViewSet):
+    
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
